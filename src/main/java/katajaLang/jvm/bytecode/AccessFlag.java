de5d76b5843e67983151974be66d64e09ac17d1f
XaverWeste <westxave@kkg-do.eu>
2024-09-03 21:13:39 +0200

    init

---
+++ src/main/java/katajaLang/jvm/bytecode/AccessFlag.java
@@ -0,0 +1,23 @@
+package katajaLang.jvm.bytecode;
+
+public interface AccessFlag {
+    int PUBLIC       = 0x0001;
+    int PRIVATE      = 0x0002;
+    int PROTECTED    = 0x0004;
+    int STATIC       = 0x0008;
+    int FINAL        = 0x0010;
+    int SUPER        = 0x0020;
+    int SYNCHRONIZED = 0x0020;
+    int VOLATILE     = 0x0040;
+    int BRIDGE       = 0x0040;
+    int TRANSIENT    = 0x0080;
+    int VARARGS      = 0x0080;
+    int NATIVE       = 0x0100;
+    int INTERFACE    = 0x0200;
+    int ABSTRACT     = 0x0400;
+    int STRICT       = 0x0800;
+    int SYNTHETIC    = 0x1000;
+    int ANNOTATION   = 0x2000;
+    int ENUM         = 0x4000;
+    int MODULE       = 0x8000;
+}
